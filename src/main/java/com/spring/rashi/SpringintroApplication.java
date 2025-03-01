d548b72a74cebe4378781047d69c5a2784690ebb
Rashi123ab <rashia729@gmail.com>
2025-03-01 19:28:05 +0530

    pass name as query parameter

--- src/main/java/com/spring/rashi/SpringintroApplication.java
+++ src/main/java/com/spring/rashi/SpringintroApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringintroApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringintroApplication.class, args);
 	}
 
--- src/main/java/com/spring/rashi/controller/HelloController.java
+++ src/main/java/com/spring/rashi/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.spring.rashi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/hello")
@@ -9,5 +10,9 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz!!";
+    }
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
