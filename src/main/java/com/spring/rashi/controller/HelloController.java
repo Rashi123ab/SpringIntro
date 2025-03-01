23303832d9a88ef041e4cd9cfcaf70a05f730a61
Rashi123ab <rashia729@gmail.com>
2025-03-01 19:35:31 +0530

    pass name as path variable

--- src/main/java/com/spring/rashi/controller/HelloController.java
+++ src/main/java/com/spring/rashi/controller/HelloController.java
@@ -3,6 +3,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/hello")
@@ -13,6 +14,10 @@ public class HelloController {
     }
     @GetMapping("/query")
     public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+    @GetMapping("/param/{name}")
+    public String sayHelloWithPathVar(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
 }
