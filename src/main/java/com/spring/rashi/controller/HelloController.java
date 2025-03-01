0c4ba4691308ae5c658d0e7f92c3b36832609c13
Rashi123ab <rashia729@gmail.com>
2025-03-01 19:48:48 +0530

    use Post req method and pass first and last name in body

--- src/main/java/com/spring/rashi/controller/HelloController.java
+++ src/main/java/com/spring/rashi/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -20,4 +21,32 @@ public class HelloController {
     public String sayHelloWithPathVar(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
+    @PostMapping("/post")
+    public String sayHelloWithPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
 }
+
+class User {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+//url--for post request in postman
+//http://localhost:8080/hello/post
+//o/p---- Hello Rashi Agrawal from BridgeLabz.
